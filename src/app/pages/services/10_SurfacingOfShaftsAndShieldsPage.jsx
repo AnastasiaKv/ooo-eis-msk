14e128ffeda5b22d5242ff5a00fbd2c722c8ef4b
vinogradov <orozov@bk.ru>
2022-09-28 15:31:01 +0300

    - Добавлены страницы услуг:   9. ТО и ремонт пассажирских вагонов   10. Наплавка валов и щитов

--- src/app/pages/services/10_SurfacingOfShaftsAndShieldsPage.jsx
+++ src/app/pages/services/10_SurfacingOfShaftsAndShieldsPage.jsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 export default function SurfacingOfShaftsAndShieldsPage() {
-  return <h2>Manatee</h2>;
+ return (
+  <div>
+   <Box
+    sx={{
+     float: 'right' /* Выравнивание по правому краю  */,
+     margin: '7px 0 7px 7px' /* Отступы вокруг картинки */,
+    }}
+    component='img'
+    alt='Изображение неведомой детали'
+   />
+   <h2>
+    <Typography variant='caption'>Наплавка валов и щитов</Typography>
+   </h2>
+   <h4>
+    <Typography variant='caption'>от 4000 руб.</Typography>
+   </h4>
+   <br />
+   <br />
+   <p>
+    <Typography variant='caption'>Пример наших работ:</Typography>
+   </p>
+   <ul>
+    <li>Восстановление вала</li>
+    <li>Восстановление шлицов</li>
+    <li>Восстановление шпоночного паза</li>
+    <li>Наплавка валов</li>
+    <li>Ремонт вала</li>
+   </ul>
+  </div>
+ );
 }
--- src/app/pages/services/9_MaintenanceAndRepairOfPassengerCarsPage.jsx
+++ src/app/pages/services/9_MaintenanceAndRepairOfPassengerCarsPage.jsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 export default function MaintenanceAndRepairOfPassengerCarsPage() {
-  return <h2>Manatee</h2>;
+ return (
+  <div>
+   <h2>
+    <Typography variant='caption'>ТО и ремонт электрооборудования пассажирских вагонов</Typography>
+   </h2>
+   <Box
+    sx={{
+     float: 'right' /* Выравнивание по правому краю  */,
+     margin: '7px 0 7px 7px' /* Отступы вокруг картинки */,
+    }}
+    component='img'
+    alt='Изображение неведомой детали'
+   />
+   <br />
+   <br />
+
+   <ul>
+    <li>
+     <Typography variant='caption'>
+      Наша фирма занимается обслуживанием всех видов ватного электрооборудования.
+     </Typography>
+    </li>
+    <li>
+     <Typography variant='caption'>Являемся подрядчиками 000 РЖД и 000 ФПК.</Typography>
+    </li>
+    <li>
+     <Typography variant='caption'>
+      Мы можем ремантировать все узлы и агрегаты на вагонах поездов.
+     </Typography>
+    </li>
+   </ul>
+  </div>
+ );
 }
