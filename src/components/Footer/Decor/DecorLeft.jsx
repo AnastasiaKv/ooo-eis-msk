d8de92ca6eb493713c1fbca26ec444a8489c5862
vinogradov <orozov@bk.ru>
2022-11-09 17:30:21 +0300

    hotfix's

---
+++ src/components/Footer/Decor/DecorLeft.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import image from '././../../../assets/img/Decor/left.png';
+import styled from 'styled-components';
+import './DecorLeft.css';
+
+export default function DecorLeft() {
+ return <img width='90' className='leftbox' src={image} alt='DecorLeft' />;
+}
---
+++ src/components/Footer/Decor/RedLine.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './RedLine.css';
+
+export default function RedLine() {
+ return <div className='RedLine'></div>;
+}
