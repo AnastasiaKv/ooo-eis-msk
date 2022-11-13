b404023c21e7a13119b6f83d8308b0e90554de75
vinogradov <orozov@bk.ru>
2022-11-13 17:08:31 +0300

    Удаление лишнего из App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,20 +5,6 @@ import '../assets/css/App.css';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import TopMediaBlock from '../layouts/TopMediaBlockLayout';
-
-import RepairMotors from '../components/OurServices/pages/1_RepairMotorsPage';
-import GeneratorRepair from '../components/OurServices/pages/2_GeneratorRepairPage';
-import RepairTransformers from '../components/OurServices/pages/3_RepairTransformersPage';
-import RefrigerationMachineRepair from '../components/OurServices/pages/4_RefrigerationMachineRepairPage';
-import RewindingMotors from '../components/OurServices/pages/5_RewindingMotorsPage';
-import RepairOfElectricHeaters from '../components/OurServices/pages/6_RepairOfElectricHeatersPage';
-import SaleOfElectricalComponents from '../components/OurServices/pages/7_SaleOfElectricalComponentsPage';
-import RepairOfBoardsAndBlocks from '../components/OurServices/pages/8_RepairOfBoardsAndBlocksPage';
-import MaintenanceAndRepairOfPassengerCars from '../components/OurServices/pages/9_TORepairOfPassengerCarsPage';
-import SurfacingOfShaftsAndShields from '../components/OurServices/pages/10_SurfacingOfShaftsAndShieldsPage';
-
-import Prices from '../pages/Prices';
-import News from '../pages/News';
 import SEO from '../components/SEO';
 
 const helmetContext = {};
