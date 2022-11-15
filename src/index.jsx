42cafeb8886fae2ce8b67adb1372bbd670eb9900
vinogradov <orozov@bk.ru>
2022-11-15 23:48:26 +0300

    hotfix's

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './app/core/reports/reportWebVitals';
-import * as serviceWorker from './app/core/serviceWorker';
+import registerServiceWorker from './app/core/registerServiceWorker';
 import App from './app/App';
 import './assets/fonts/Furore.otf';
 import './assets/css/index.css';
@@ -13,19 +13,24 @@ import {hydrate, render} from 'react-dom';
 const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
  hydrate(
-  <LayoutProvider>
-   <App />
-  </LayoutProvider>,
-  rootElement
+  <React.StrictMode>
+   <LayoutProvider>
+    <App />
+   </LayoutProvider>
+  </React.StrictMode>,
+  rootElement,
+  rootElement.firstElementChild
  );
 } else {
  render(
-  <LayoutProvider>
-   <App />
-  </LayoutProvider>,
+  <React.StrictMode>
+   <LayoutProvider>
+    <App />
+   </LayoutProvider>
+  </React.StrictMode>,
   rootElement
  );
 }
 console.log(process.env);
 reportWebVitals(console.log);
-serviceWorker.register();
+registerServiceWorker();
