41b5d983937ebc1ea7aaa391b9bc1be591943b18
vinogradov <orozov@bk.ru>
2022-11-25 14:43:35 +0300

    Добавлен спиннер загрузки

--- src/app/layouts/LayoutProvider.jsx
+++ src/app/layouts/LayoutProvider.jsx
@@ -2,10 +2,13 @@ import React, {useEffect} from 'react';
 import LoadingSplashCircular from '../modules/shared/LoadingSplashCircular';
 
 const disableSplashScreen = () => {
- const splashScreen = document.getElementById('splash');
+ const splashScreen = document.getElementById('loading');
  if (splashScreen) {
   splashScreen.style.setProperty('display', 'none');
  }
+
+ 
+
 };
 
 export default function LayoutProvider({children}) {
