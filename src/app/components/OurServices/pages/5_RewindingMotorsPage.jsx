4daef7b7ba37ecdd7675a60a6bb661f7b362dde9
vinogradov <orozov@bk.ru>
2022-11-27 02:39:10 +0300

    Исправлено отображение на ios_saf 16.1.1 услуга №5

--- src/app/components/OurServices/pages/5_RewindingMotorsPage.jsx
+++ src/app/components/OurServices/pages/5_RewindingMotorsPage.jsx
@@ -5,7 +5,7 @@ import {Typography} from '@material-ui/core';
 import CustomizedTables from '../../../modules/common/CustomizedTables';
 import image5 from '../../../../assets/img/services/5.png';
 import Paper from '@mui/material/Paper';
-import Bolt from '@mui/icons-material/Bolt';
+import BoltSvg from '@mui/icons-material/Bolt';
 import SEO from '../../SEO';
 
 export default function RewindingMotorsPage() {
@@ -48,115 +48,76 @@ export default function RewindingMotorsPage() {
      </p>
      <br />
      <ul style={{color: '#1a202c', textAlign: 'left', listStyle: 'none'}}>
-      <h6>
-       <Typography variant='h6' style={{paddingRight: '20px', paddingBottom: '0px'}}>
-        При выходе из строя электрообмоток производятся следующие работы:
-       </Typography>
-      </h6>
-      <Stack direction='row' spacing={3} sx={{paddingTop: '1em'}}>
-       <Bolt
-        style={{
-         margin: '0px 0px 0px 0px',
-         width: '25px',
-         height: '26px',
-         color: '	#ffcf40',
-        }}
-       />
+      <p style={{paddingBottom: '1em'}}>
+       <span style={{fontSize: 19, paddingRight: '12em', paddingBottom: '1em'}}>
+        <b>При выходе из строя электрообмоток производятся следующие работы:</b>
+       </span>
+      </p>
+
+      <div
+       style={{display: 'flex', alignItems: 'center', justifyContent: 'left', paddingBottom: 7}}
+      >
+       <BoltSvg style={{width: '25px', height: '26px', color: '#ffcf40'}} />
        <Typography variant='caption' component='span' style={{fontSize: '16px', marginLeft: 0}}>
         Разборка и очистка
        </Typography>
-      </Stack>
-      <Stack direction='row' spacing={3}>
-       <Bolt
-        style={{
-         margin: '0px 0px 0px 0px',
-         width: '25px',
-         height: '26px',
-         color: '	#ffcf40',
-        }}
-       />
+      </div>
+      <div
+       style={{display: 'flex', alignItems: 'center', justifyContent: 'left', paddingBottom: 7}}
+      >
+       <BoltSvg style={{width: '25px', height: '26px', color: '#ffcf40'}} />
        <Typography variant='caption' component='span' style={{fontSize: '16px', marginLeft: 0}}>
         Расчет и подбор материалов для обмотки
        </Typography>
-      </Stack>
-      <Stack direction='row' spacing={3}>
-       <Bolt
-        style={{
-         margin: '0px 0px 0px 0px',
-         width: '25px',
-         height: '26px',
-         color: '	#ffcf40',
-        }}
-       />
+      </div>
+      <div
+       style={{display: 'flex', alignItems: 'center', justifyContent: 'left', paddingBottom: 7}}
+      >
+       <BoltSvg style={{width: '25px', height: '26px', color: '#ffcf40'}} />
        <Typography variant='caption' component='span' style={{fontSize: '16px', marginLeft: 0}}>
         Устранение дефектов ротора и/или статора с обработкой пазов
        </Typography>
-      </Stack>
-      <Stack direction='row' spacing={3}>
-       <Bolt
-        style={{
-         margin: '0px 0px 0px 0px',
-         width: '25px',
-         height: '26px',
-         color: '	#ffcf40',
-        }}
-       />
+      </div>
+      <div
+       style={{display: 'flex', alignItems: 'center', justifyContent: 'left', paddingBottom: 7}}
+      >
+       <BoltSvg style={{width: '25px', height: '26px', color: '#ffcf40'}} />
        <Typography variant='caption' component='span' style={{fontSize: '16px', marginLeft: 0}}>
         Намотка катушек
        </Typography>
-      </Stack>
-      <Stack direction='row' spacing={3}>
-       <Bolt
-        style={{
-         margin: '0px 0px 0px 0px',
-         width: '25px',
-         height: '26px',
-         color: '	#ffcf40',
-        }}
-       />
+      </div>
+      <div
+       style={{display: 'flex', alignItems: 'center', justifyContent: 'left', paddingBottom: 7}}
+      >
+       <BoltSvg style={{width: '25px', height: '26px', color: '#ffcf40'}} />
        <Typography variant='caption' component='span' style={{fontSize: '16px', marginLeft: 0}}>
         Подготовка изоляции с последующей установкой обмотки
        </Typography>
-      </Stack>
-      <Stack direction='row' spacing={3}>
-       <Bolt
-        style={{
-         margin: '0px 0px 0px 0px',
-         width: '25px',
-         height: '26px',
-         color: '	#ffcf40',
-        }}
-       />
+      </div>
+      <div
+       style={{display: 'flex', alignItems: 'center', justifyContent: 'left', paddingBottom: 7}}
+      >
+       <BoltSvg style={{width: '25px', height: '26px', color: '#ffcf40'}} />
        <Typography variant='caption' component='span' style={{fontSize: '16px', marginLeft: 0}}>
         Пропитка токоведущих элементов
        </Typography>
-      </Stack>
-      <Stack direction='row' spacing={3}>
-       <Bolt
-        style={{
-         margin: '0px 0px 0px 0px',
-         width: '25px',
-         height: '26px',
-         color: '	#ffcf40',
-        }}
-       />
+      </div>
+      <div
+       style={{display: 'flex', alignItems: 'center', justifyContent: 'left', paddingBottom: 7}}
+      >
+       <BoltSvg style={{width: '25px', height: '26px', color: '#ffcf40'}} />
        <Typography variant='caption' component='span' style={{fontSize: '16px', marginLeft: 0}}>
         Просушка изоляции и тестирование обмоток
        </Typography>
-      </Stack>
-      <Stack direction='row' spacing={3}>
-       <Bolt
-        style={{
-         margin: '0px 0px 0px 0px',
-         width: '25px',
-         height: '26px',
-         color: '	#ffcf40',
-        }}
-       />
+      </div>
+      <div
+       style={{display: 'flex', alignItems: 'center', justifyContent: 'left'}}
+      >
+       <BoltSvg style={{width: '25px', height: '26px', color: '#ffcf40'}} />
        <Typography variant='caption' component='span' style={{fontSize: '16px', marginLeft: 0}}>
         Сборка и испытание электродвигателя
        </Typography>
-      </Stack>
+      </div>
      </ul>
      <CustomizedTables />
     </Stack>
