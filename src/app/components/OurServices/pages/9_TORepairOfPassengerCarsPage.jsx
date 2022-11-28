7ae1d9859b7f04e6b9982b9a6987bad4a7f57a78
vinogradov <orozov@bk.ru>
2022-11-28 04:40:02 +0300

    Исправлено отображение на ios_saf 16.1.1 услуга №9

--- src/app/components/OurServices/pages/9_TORepairOfPassengerCarsPage.jsx
+++ src/app/components/OurServices/pages/9_TORepairOfPassengerCarsPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import {Typography} from '@material-ui/core';
-import GppGoodRoundedIcon from '@mui/icons-material/GppGoodRounded';
+import GppGoodSvg from '@mui/icons-material/GppGoodRounded';
 import image9 from '../../../../assets/img/services/9_1.png';
 import Paper from '@mui/material/Paper';
 import SEO from '../../SEO';
@@ -35,112 +35,90 @@ export default function TORepairOfPassengerCarsPage() {
        ТО и ремонт электрооборудования пассажирских вагонов
       </Typography>
      </h1>
-     <Stack
-      direction='row-reverse'
-      sx={{
-       paddingTop: '3em',
-       display: 'flex',
-       textAlign: 'justify',
-       alignItems: 'center',
-       maxWidth: '800px',
-       fontSize: 15,
-       lineHeight: '1.5',
-       color: '#292929',
-      }}
-     >
-      <Stack direction='row' sx={{paddingRight: '3em'}}>
-       <Box
-        sx={{
-         borderRadius: '7px',
-         height: '19em',
-         width: '25em',
-         float: 'right' /* Выравнивание по правому краю  */,
-         margin: '0px -22px 29px 9px' /* Отступы вокруг картинки */,
-        }}
-        loading='lazy'
-        component='img'
-        alt='ТО и ремонт электрооборудования пассажирских вагонов'
-        src={image9}
-       />
-      </Stack>
-      <ul>
-       <Stack
-        sx={{
-         marginRight: '2em',
-         color: '#1a202c',
+     <br />
+     <br />
+     <Stack direction='row-reverse' sx={{paddingRight: '3em'}}>
+      <Box
+       sx={{
+        borderRadius: '7px',
+        height: '11em',
+        width: '38em',
+        float: 'right' /* Выравнивание по правому краю  */,
+        margin: '-7px -22px 29px 85px' /* Отступы вокруг картинки */,
+       }}
+       loading='lazy'
+       component='img'
+       alt='ТО и ремонт электрооборудования пассажирских вагонов'
+       src={image9}
+      />
+
+      <Stack component='ul' spacing={3} style={{textAlign: 'left'}}>
+       <Typography
+        variant='body2'
+        component='p'
+        style={{
+         fontFamily: 'Roboto',
+         paddingLeft: '3px',
+         fontSize: '16px',
+         marginLeft: '11px',
+         textAlign: '-webkit-auto',
         }}
        >
-        <Stack direction='row' spacing={3} sx={{fontFamily: 'Roboto', paddingBottom: '3em'}}>
-         <GppGoodRoundedIcon
-          style={{
-           margin: '0px 0px 0px 0px',
-           width: '25px',
-           height: '26px',
-           color: 'green',
-          }}
-         />
-         <Typography
-          variant='body2'
-          component='p'
-          style={{
-           fontFamily: 'Roboto',
-           paddingLeft: '3px',
-           fontSize: '16px',
-           marginLeft: '11px',
-           textAlign: '-webkit-auto',
-          }}
-         >
-          Наша фирма занимается обслуживанием всех видов ватного электрооборудования.
-         </Typography>
-        </Stack>
-        <Stack direction='row' spacing={3} sx={{paddingBottom: '3em'}}>
-         <GppGoodRoundedIcon
-          style={{
-           margin: '0px 0px 0px 0px',
-           width: '25px',
-           height: '26px',
-           color: 'green',
-          }}
-         />
-         <Typography
-          variant='body2'
-          component='p'
-          style={{
-           fontFamily: 'Roboto',
-           paddingLeft: '3px',
-           fontSize: '16px',
-           marginLeft: '11px',
-           textAlign: '-webkit-auto',
-          }}
-         >
-          Являемся подрядчиками ООО РЖД и ООО ФПК.
-         </Typography>
-        </Stack>
-        <Stack direction='row' spacing={3} sx={{}}>
-         <GppGoodRoundedIcon
-          style={{
-           margin: '0px 0px 0px 0px',
-           width: '25px',
-           height: '26px',
-           color: 'green',
-          }}
-         />
-         <Typography
-          variant='body2'
-          component='p'
-          style={{
-           fontFamily: 'Roboto',
-           paddingLeft: '3px',
-           fontSize: '16px',
-           marginLeft: '11px',
-           textAlign: '-webkit-auto',
-          }}
-         >
-          Мы можем ремантировать все узлы и агрегаты на вагонах поездов.
-         </Typography>
-        </Stack>
-       </Stack>
-      </ul>
+        <GppGoodSvg
+         style={{
+          margin: '0px 10px -8px 0em',
+          width: '25px',
+          height: '26px',
+          color: 'green',
+         }}
+        />
+        Наша фирма занимается обслуживанием всех видов ватного электрооборудования.
+       </Typography>
+
+       <Typography
+        variant='body2'
+        component='p'
+        style={{
+         fontFamily: 'Roboto',
+         paddingLeft: '3px',
+         fontSize: '16px',
+         marginLeft: '11px',
+         textAlign: '-webkit-auto',
+        }}
+       >
+        <GppGoodSvg
+         style={{
+          margin: '0px 10px -8px 0em',
+          width: '25px',
+          height: '26px',
+          color: 'green',
+         }}
+        />
+        Являемся подрядчиками ООО РЖД и ООО ФПК.
+       </Typography>
+
+       <Typography
+        variant='body2'
+        component='p'
+        style={{
+         fontFamily: 'Roboto',
+         paddingLeft: '3px',
+         fontSize: '16px',
+         marginLeft: '11px',
+         textAlign: '-webkit-auto',
+        }}
+       >
+        <GppGoodSvg
+         style={{
+          margin: '0px 10px -8px 0em',
+          width: '25px',
+          height: '26px',
+          color: 'green',
+         }}
+        />
+        Мы можем ремантировать все узлы и агрегаты на вагонах поездов.
+       </Typography>
+      </Stack>
      </Stack>
     </Stack>
    </Paper>
