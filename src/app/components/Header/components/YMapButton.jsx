ac49818138a91a90af87db9898202623d66228fc
vinogradov <orozov@bk.ru>
2022-12-05 18:49:25 +0300

    Добавлен transition  эффект на я.карту

--- src/app/components/Header/components/YMapButton.jsx
+++ src/app/components/Header/components/YMapButton.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import MapIcon from '@mui/icons-material/Map';
-import {Typography, Stack} from '@mui/material';
+import {Typography, Stack, Slide} from '@mui/material';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -8,12 +8,19 @@ import DialogContent from '@mui/material/DialogContent';
 /* import DialogContentText from '@mui/material/DialogContentText'; */
 import DialogTitle from '@mui/material/DialogTitle';
 import axios from 'axios';
-import {Formik, Form/* , Field, ErrorMessage */} from 'formik';
+import {Formik, Form /* , Field, ErrorMessage */} from 'formik';
 /* import {Stack, width} from '@mui/system'; */
 import {createTheme, ThemeProvider} from '@mui/material/styles';
-import {YMaps, Map, Placemark, GeolocationControl/* , RouteButton */} from '@pbe/react-yandex-maps';
+import {
+ YMaps,
+ Map,
+ Placemark,
+ GeolocationControl /* , RouteButton */,
+} from '@pbe/react-yandex-maps';
 /* import {Paper} from '@mui/material'; */
-
+const SlideTransition = React.forwardRef(function Transition(props, ref) {
+ return <Slide direction='up' ref={ref} {...props} />;
+});
 const theme = createTheme({
  components: {
   // todo: remove once the issue is addressed: https://github.com/mui/material-ui/issues/31185
@@ -50,12 +57,12 @@ function YMap() {
 }
 
 function YMapButton(props) {
-/*  const {classes} = props; */
+ /*  const {classes} = props; */
  const [open, setOpen] = useState(false);
-/*  const [isSubmitionCompleted, setSubmitionCompleted] = useState(false); */
+ /*  const [isSubmitionCompleted, setSubmitionCompleted] = useState(false); */
 
-/*  const [isShown, setIsShown] = useState(false); */
-/*  const [fullWidth, setFullWidth] = useState(true); */
+ /*  const [isShown, setIsShown] = useState(false); */
+ /*  const [fullWidth, setFullWidth] = useState(true); */
 
  /* const handleClick = (event) => {
   // 👇️ toggle shown state
@@ -71,7 +78,7 @@ function YMapButton(props) {
  }
 
  function handleClickOpen() {
-/*   setSubmitionCompleted(false); */
+  /*   setSubmitionCompleted(false); */
   setOpen(true);
  }
 
@@ -94,84 +101,94 @@ function YMapButton(props) {
     >
      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Карта&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     </Button>
-    <Dialog maxWidth='md' open={open} onClose={handleClose} aria-labelledby='form-dialog-title' disableScrollLock
-    PaperProps={{sx: {left: 0,},}}>
-     {/* !isShown */1 && (
-      <Stack sx={{backgroundColor: '#2d3748', color: '#FFFFFFFF', flexGrow: 1}}>
-       <DialogTitle style={{marginTop: '-0.5em', marginBottom: '-0.5em'}} id='form-dialog-title'>
-        <Typography
-         variant='h4'
-         component='h1'
-         style={{
-          fontFamily: 'Furore',
-          color: '#FFFFFFFF',
-          flexGrow: 1,
-          marginTop: '-0.2em',
-          marginBottom: '-0.1em',
-         }}
-        >
-         Наш офис в Москве
-        </Typography>
-       </DialogTitle>
-       <ThemeProvider theme={theme}>
-        <DialogContent>
-         {/* <DialogContentText>Оставьте свои данные - и мы Вам перезвоним!</DialogContentText> */}
-         <Formik
-          onSubmit={(values, {setSubmitting}) => {
-           setSubmitting(true);
-           axios
-            .post(contactFormEndpoint, values, {
-             headers: {
-              'Access-Control-Allow-Origin': '*',
-              'Content-Type': 'application/json',
-             },
-            })
-            .then((resp) => {
-              setSubmitting(false);
-            });
+    <Dialog
+     maxWidth='md'
+     open={open}
+     onClose={handleClose}
+     aria-labelledby='form-dialog-title'
+     disableScrollLock
+     TransitionComponent={SlideTransition}
+     transitionDuration={{appear: 0, enter: 350, exit: 350}}
+     PaperProps={{sx: {left: 0}}}
+    >
+     {
+      /* !isShown */ 1 && (
+       <Stack sx={{backgroundColor: '#2d3748', color: '#FFFFFFFF', flexGrow: 1}}>
+        <DialogTitle style={{marginTop: '-0.5em', marginBottom: '-0.5em'}} id='form-dialog-title'>
+         <Typography
+          variant='h4'
+          component='h1'
+          style={{
+           fontFamily: 'Furore',
+           color: '#FFFFFFFF',
+           flexGrow: 1,
+           marginTop: '-0.2em',
+           marginBottom: '-0.1em',
           }}
          >
-          {(props) => {
-           const {
-            values,
-            touched,
-            errors,
-            dirty,
-            isSubmitting,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            handleReset,
-           } = props;
-           return (
-            <Form onSubmit={handleSubmit}>
-             <Stack
-              sx={{
-               overflow: 'hidden',
-               marginTop: '-15px',
-               marginLeft: '-5.3em',
-               marginRight: '-23px',
-               marginBottom: '-13px',
-               padding: '0px 0em 10px 0em',
-               flexDirection: 'column',
-               alignItems: 'baseline',
-              }}
-              direction='row'
-              spacing={0}
-              noValidate
-              component='form'
-             >
-              <YMap />
-             </Stack>
-             <DialogActions style={{}}>{/* <DisplayFormikState {...props} /> */}</DialogActions>
-            </Form>
-           );
-          }}
-         </Formik>
-        </DialogContent>
-       </ThemeProvider>
-      </Stack>
-     )}
+          Наш офис в Москве
+         </Typography>
+        </DialogTitle>
+        <ThemeProvider theme={theme}>
+         <DialogContent>
+          {/* <DialogContentText>Оставьте свои данные - и мы Вам перезвоним!</DialogContentText> */}
+          <Formik
+           onSubmit={(values, {setSubmitting}) => {
+            setSubmitting(true);
+            axios
+             .post(contactFormEndpoint, values, {
+              headers: {
+               'Access-Control-Allow-Origin': '*',
+               'Content-Type': 'application/json',
+              },
+             })
+             .then((resp) => {
+              setSubmitting(false);
+             });
+           }}
+          >
+           {(props) => {
+            const {
+             values,
+             touched,
+             errors,
+             dirty,
+             isSubmitting,
+             handleChange,
+             handleBlur,
+             handleSubmit,
+             handleReset,
+            } = props;
+            return (
+             <Form onSubmit={handleSubmit}>
+              <Stack
+               sx={{
+                overflow: 'hidden',
+                marginTop: '-15px',
+                marginLeft: '-5.3em',
+                marginRight: '-23px',
+                marginBottom: '-13px',
+                padding: '0px 0em 10px 0em',
+                flexDirection: 'column',
+                alignItems: 'baseline',
+               }}
+               direction='row'
+               spacing={0}
+               noValidate
+               component='form'
+              >
+               <YMap />
+              </Stack>
+              <DialogActions style={{}}>{/* <DisplayFormikState {...props} /> */}</DialogActions>
+             </Form>
+            );
+           }}
+          </Formik>
+         </DialogContent>
+        </ThemeProvider>
+       </Stack>
+      )
+     }
     </Dialog>
    </div>
   </React.Fragment>
